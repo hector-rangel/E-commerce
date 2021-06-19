07557d02388e9b2f1d7f8818d5c16f48f9c4d517
Hector Rangel <hector.rangeljr@yahoo.com>
2021-06-19 17:43:42 -0500

    finished routes except tag-routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -6,6 +6,29 @@ const { Tag, Product, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all tags
   // be sure to include its associated Product data
+  Tag.findAll({
+    attributes: [
+      'id',
+      'tag_name'
+    ],
+    include: {
+      model: Product,
+      attributes: [
+        'id',
+        'product_name',
+        'price',
+        'stock',
+        'category_id'
+      ],
+      through: Tag,
+      as: 'tag_product'
+    }
+  })
+    .then(dbTagData => res.json(dbTagData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 router.get('/:id', (req, res) => {
